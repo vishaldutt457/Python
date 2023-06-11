bb5555b13899050b0e0e9871c2f8697a711997d7
vishalDutt <duttvishal689@gmail.com>
2023-06-11 13:52:43 +0530

    sets Method

--- setmethod.py
+++ setmethod.py
@@ -37,7 +37,7 @@ print(s3)
 s1.update(s2)
 print(s1)
 
-# result------------  {1, 2, 3, 5, 6, 7} {3, 6, 7}
+# result------------  {1, 2, 3, 5, 6, 7} 
 
 
 ##################Intersection Method------
@@ -55,6 +55,9 @@ print(cities)
 # result------------  {'Tokyo', 'Madrid'}
 
 
+##################Symmetric Method------
+
+
 cities3=cities.symmetric_difference(cities2)
 print(cities3)
 
@@ -64,3 +67,110 @@ cities.symmetric_difference_update(cities2)
 print(cities)
 
 # result------------  {'Berlin', 'Delhi', 'Kabul', 'Seoul'}
+
+
+##################Difference Method------
+
+cities3=cities.difference(cities2)
+print(cities3)
+
+# result------------   {'Berlin', 'Delhi'}
+
+
+cities.difference_update(cities2)
+print(cities3)
+
+# result------------   {'Berlin', 'Delhi'}
+
+
+##################isdisjoint Method------
+
+
+#if even single value is common will return false else true
+values1={"vishal","arjun","shubham","vikas"}
+values2={"vishal","nishtha","sakshi","arjun"}
+
+print(values1.isdisjoint(values2));
+
+# result------------   False
+
+
+values3={"vishal1","arjun1","shubham","vikas"}
+values4={"vishal","nishtha","sakshi","arjun"}
+
+print(values3.isdisjoint(values4))
+
+# result------------   True
+
+
+################## issuperset Method------
+
+
+#if values of child set are present in parent set then return true else false
+values3={"vishal","arjun","shubham","vikas"}
+values4={"nishtha","sakshi"}
+
+print(values3.issuperset(values4))
+
+# result------------   False
+
+
+values3={"vishal","arjun","shubham","vikas"}
+values4={"vishal","arjun"}
+
+
+print(values3.issuperset(values4))
+
+#result------------   True
+
+
+################## isubset Method------
+
+values3={"vishal","arjun","shubham","vikas"}
+values4={"vishal","arjun"}
+
+print(values4.issubset(values3))
+
+#result------------   True
+
+
+################## add Method------
+
+data1={"r1","r2","r3","r4"}
+data1.add("helsinki")
+print(data1)
+
+
+################## update Method------
+
+
+data1={"r1","r2","r3","r4"}
+data1.update({"helsinki","tokyo","berlin"})
+print(data1)
+
+
+################## remove/discard Method------
+
+
+data1={"r1","r2","r3","r4"}
+data1.discard("r3")
+print(data1)
+
+
+################## pop Method------
+
+
+data1={"r1","r2","r3","r4"}
+data1.pop()
+print(data1)
+
+
+################## clear Method------
+
+data1={"r1","r2","r3","r4"}
+data1.clear()
+print(data1)
+
+
+
+
